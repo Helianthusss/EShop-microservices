7aabc51216cfafc1df4534d7eb95ecea5fc23640
Helianthusss <anhtrankoc@gmail.com>
2024-05-29 14:02:22 +0700

    initial building blocks

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,30 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+: ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public class CreateProductHandler
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        // create Product entity from command object
+        // save database
+        // return CreateProductResult result\
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        //save to database
+        //return result
+        return new CreateProductResult(product.Id);
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,8 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddCarter();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipline.
+app.MapCarter();
 
 app.Run();
